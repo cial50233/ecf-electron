5ea1e5de3ae2b21b9e49b8892653a173d1b0321f
Cial TRIN <cial50233@gmail.com>
2021-03-18 15:50:17 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,45 @@
+async function doFetch(endpoint) {
+    try {
+        const resp = await fetch("https://restcountries.eu/rest/v2/alpha/" + endpoint, { method: 'GET' });
+        if (resp.ok) {
+            return await resp.json();
+        } else {
+            return `error fetching endpoint: /${endpoint}`;
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function display(data, selector) {
+    const container = document.querySelector(selector);
+    container.innerHTML = data.content;
+    container.classList.add('be-bold');
+}
+
+async function fetchA() {
+    const data = await doFetch('AF');
+    display(data.name, '#component-a');
+}
+
+async function fetchB() {
+    const data = await doFetch('AX');
+    display(data, '#component-b');
+}
+
+async function fetchC() {
+    const data = await doFetch('AL');
+    display(data, '#component-c');
+}
+
+async function fetchD() {
+    const data = await doFetch('DZ');
+    display(data, '#component-d');
+}
+
+function fetchAndDisplayData() {
+    fetchA();
+    fetchB();
+    fetchC();
+    fetchD();
+}
